1535db7a1422aedbd6a61a3f291b785f16507ee2
brunoocastro <bruno.scastro2012@hotmail.com>
2021-12-18 12:24:43 -0300

    config: adjusting plugins to build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,15 @@ import Head from 'next/head'
 
 const Home: React.FC = () => {
   return (
-    <div className="bg-blue-900">
+    <div className="bg-blue-900 min-h-screen min-w-screen">
       <Head>
         <title>Flow Badges</title>
       </Head>
 
       <main>
-        <h1 className="text-yellow-400 text-center">Seja vem vindo ao Flow badges</h1>
+        <h1 className="text-yellow-400 font-serif text-center">
+          Seja vem vindo ao Flow badges
+        </h1>
       </main>
     </div>
   )
