61c82a08cf2a69ed89f5be32f76702711c6b48ad
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-12-26 00:51:05 -0300

    chore: add mobile layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,76 @@
 import Link from 'next/link'
+import {
+  FaGithub,
+  FaTwitch,
+  FaTwitter,
+  FaTelegram,
+  FaWhatsapp
+} from 'react-icons/fa'
+
 import { Logo } from '../Logo'
 
 export const Footer = () => {
   return (
     <footer className="border-gradient pt-[1px]">
       <div className="bg-base-background relative">
-        <div className="flex justify-between items-center max-w-4xl mx-auto text-white py-4">
+        <div className="flex flex-col md:flex-row md:flex-none gap-5 md:gap-0 justify-between items-center max-w-4xl mx-auto text-white py-4">
           <Link href="/">
             <a>
               <Logo color="white" width="91" height="26" />
             </a>
           </Link>
+          <ul className="md:hidden flex gap-4 items-center text-black">
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href="https://github.com/brunoocastro/flowbadges"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaGithub aria-label="GitHub" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaWhatsapp aria-label="Whatsapp" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaTelegram aria-label="Telegram" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaTwitter aria-label="Twitter" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href="https://www.twitch.tv/tonelive"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaTwitch aria-label="Twitch" size={20} />
+              </a>
+            </li>
+          </ul>
           <p className="text-xs">
             Um projeto da <strong className="font-bold">comunidade</strong> para
             a <strong className="font-bold">comunidade</strong>.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,106 +30,108 @@ const Home: React.FC = () => {
       coverUrl: '/assets/profile-stats-cover.png'
     }
   ]
+
   return (
-    <div className="bg-home bg-base-backgroundDark overflow-x-hidden">
-      <Head>
-        <title>Flow Badges</title>
-      </Head>
+    <>
+      <div className="bg-home bg-base-backgroundDark">
+        <Head>
+          <title>Flow Badges</title>
+        </Head>
 
-      <ul className="fixed right-3 top-1/2 -translate-y-1/2 space-y-4">
-        <li>
-          <a
-            className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
-            href="https://github.com/brunoocastro/flowbadges"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <FaGithub aria-label="GitHub" size={20} />
-          </a>
-        </li>
-        <li>
-          <a
-            className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
-            href=""
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <FaWhatsapp aria-label="Whatsapp" size={20} />
-          </a>
-        </li>
-        <li>
-          <a
-            className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
-            href=""
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <FaTelegram aria-label="Telegram" size={20} />
-          </a>
-        </li>
-        <li>
-          <a
-            className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
-            href=""
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <FaTwitter aria-label="Twitter" size={20} />
-          </a>
-        </li>
-        <li>
-          <a
-            className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
-            href="https://www.twitch.tv/tonelive"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <FaTwitch aria-label="Twitch" size={20} />
-          </a>
-        </li>
-      </ul>
+        <ul className="hidden md:block fixed right-3 top-1/2 -translate-y-1/2 space-y-4 text-black">
+          <li>
+            <a
+              className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+              href="https://github.com/brunoocastro/flowbadges"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaGithub aria-label="GitHub" size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+              href=""
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaWhatsapp aria-label="Whatsapp" size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+              href=""
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaTelegram aria-label="Telegram" size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+              href=""
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaTwitter aria-label="Twitter" size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+              href="https://www.twitch.tv/tonelive"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaTwitch aria-label="Twitch" size={20} />
+            </a>
+          </li>
+        </ul>
 
-      <header className="rounded-br-lg rounded-bl-lg border-gradient max-w-[250px] mx-auto px-[1px] pb-[1px] relative">
-        <div className="absolute -inset-3 bg-base-yellow-700 rounded-lg blur opacity-10" />
-        <div className="relative bg-base-background max-w-[250px] mx-auto flex justify-center items-center rounded-br-lg py-4 rounded-bl-lg">
-          <Logo color="white" width="121" height="34" />
-        </div>
-      </header>
+        <header className="rounded-br-lg rounded-bl-lg border-gradient max-w-[250px] mx-auto px-[1px] pb-[1px] relative">
+          <div className="absolute -inset-3 bg-base-yellow-700 rounded-lg blur opacity-10" />
+          <div className="relative bg-base-background max-w-[250px] mx-auto flex justify-center items-center rounded-br-lg py-4 rounded-bl-lg">
+            <Logo color="white" width="121" height="34" />
+          </div>
+        </header>
 
-      <main className="h-screen w-screen grid place-items-center">
-        <nav>
-          <ul className="grid grid-cols-[289px_289px] gap-8">
-            {links.map(link => (
-              <li
-                key={link.href}
-                className="p-[2px] border-gradient-two rounded-lg relative"
-              >
-                <div className="absolute -inset-2 bg-base-yellow-700 rounded-lg blur opacity-10" />
-                <div className="relative bg-base-background rounded-lg p-5 text-center text-white">
-                  <Image
-                    src={link.coverUrl}
-                    width={249}
-                    height={171}
-                    quality={100}
-                    alt="Imagem com emblemas do flow"
-                  />
-                  <h2 className="my-8 text-lg font-semibold">{link.title}</h2>
-                  <p className="">{link.description}</p>
-                  <Link href={link.href}>
-                    <a className="border-gradient-two w-52 h-11 flex justify-center items-center gap-3 mt-10 rounded-lg mx-auto text-base-yellow-700">
-                      Acessar
-                      <HiOutlineArrowRight />
-                    </a>
-                  </Link>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </main>
-
+        <main className="min-h-screen w-full grid place-items-center mt-10 md:mt-0 mb-10">
+          <nav>
+            <ul className="grid grid-cols-[289px] md:grid-cols-[289px_289px] gap-8">
+              {links.map(link => (
+                <li
+                  key={link.href}
+                  className="p-[2px] border-gradient-two rounded-lg relative"
+                >
+                  <div className="absolute -inset-2 bg-base-yellow-700 rounded-lg blur opacity-10" />
+                  <div className="relative bg-base-background rounded-lg p-5 text-center">
+                    <Image
+                      src={link.coverUrl}
+                      width={249}
+                      height={171}
+                      quality={100}
+                      alt="Imagem com emblemas do flow"
+                    />
+                    <h2 className="my-8 text-lg font-semibold">{link.title}</h2>
+                    <p className="">{link.description}</p>
+                    <Link href={link.href}>
+                      <a className="border-gradient-two w-52 h-11 flex justify-center items-center gap-3 mt-10 rounded-lg mx-auto text-base-yellow-700">
+                        Acessar
+                        <HiOutlineArrowRight />
+                      </a>
+                    </Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   )
 }
 
