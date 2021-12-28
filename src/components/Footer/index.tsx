b89c929ea4d1a8ca002e88aebc9c682545009664
brunoocastro <bruno.scastro2012@hotmail.com>
2021-12-27 21:12:44 -0300

    feat: Changing links to constants

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,38 +1,103 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import {
+  FaGithub,
+  FaTwitch,
+  FaTwitter,
+  FaTelegram,
+  FaWhatsapp
+} from 'react-icons/fa'
 
-const textButtons = [
-  ['Sobre', '/about'],
-  ['Quem somos?', '/contributors'],
-  ['Ferramentas', '/']
-]
+import { Logo } from '../Logo'
 
-const FooterCP: React.FC = () => (
-  <footer className="relative flex justify-around align-middle flex-col bg-base-blue-700 w-full h-24 bottom-0">
-    <div className="my-auto flex justify-around align-middle content-center text-justify ">
-      <Link href={'/'} passHref>
-        <div className="h-full w-[20%] grow-1 bg-slate-500 cursor-pointer">
-          <Image
-            src={'/assets/logo-green-white.svg'}
-            width="100%"
-            height="100%"
-            alt={'Logo do FlowBadges'}
-          />
+const Footer = () => {
+  return (
+    <footer className="border-gradient pt-[1px] relative bottom-0 w-full">
+      <div className="bg-base-background relative">
+        <div className="flex flex-col md:flex-row md:flex-none gap-5 md:gap-0 justify-between items-center max-w-4xl mx-auto text-white py-4">
+          <Link href="/">
+            <a>
+              <Logo color="white" width="91" height="26" />
+            </a>
+          </Link>
+          <ul className="md:hidden flex gap-4 items-center text-black">
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href="https://github.com/brunoocastro/flowbadges"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaGithub aria-label="GitHub" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaWhatsapp aria-label="Whatsapp" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaTelegram aria-label="Telegram" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaTwitter aria-label="Twitter" size={20} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                href="https://www.twitch.tv/tonelive"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaTwitch aria-label="Twitch" size={20} />
+              </a>
+            </li>
+          </ul>
+          <p className="text-xs">
+            Um projeto da <strong className="font-bold">comunidade</strong> para
+            a <strong className="font-bold">comunidade</strong>.
+          </p>
+          <nav>
+            <ul className="flex items-center gap-10">
+              <li>
+                <Link href="/about">
+                  <a className="underline font-semibold">Sobre nós</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/ferramentas">
+                  <a className="underline font-semibold">Ferramentas</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/participe">
+                  <a className="underline font-semibold">Participe</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </Link>
-      <p className="ml-4 text-xs h-full w-auto font-normal text-justify">
-        Um projeto da <h1 className="font-bold">comunidade</h1>
-        para a <h1 className="font-bold">comunidade.</h1>
-      </p>
-      {textButtons.map(([title, url]) => (
-        <Link href={url} passHref key={title}>
-          <h1 className="rounded-lg text-center h-full w-auto cursor-pointer text-base-white font-bold bg-yellow-300 hover:text-base-yellow-400">
-            {title}
-          </h1>
-        </Link>
-      ))}
-    </div>
-  </footer>
-)
+      </div>
+    </footer>
+  )
+}
 
-export default FooterCP
+export default Footer
--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,5 +1,9 @@
 const socialLinks = {
-  discord: 'https://discord.gg/E7kNDMm6c8'
+  github: 'https://github.com/brunoocastro/flowbadges',
+  discord: 'https://discord.gg/E7kNDMm6c8',
+  twitter: 'https://twitter.com/otonelive',
+  twitch: 'https://www.twitch.tv/tonelive',
+  whatsapp: 'https://chat.whatsapp.com/Icfir88VYt5EqhFXThaChA'
 }
 
 export default socialLinks
